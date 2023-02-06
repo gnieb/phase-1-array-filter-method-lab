0ba8a525fbd9c0470809d3c63cc47b9ee35d419c
Grace Nieboer <grace.nieboer@outlook.com>
2023-02-06 17:03:10 -0500

    tests copleted

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solution here
+
+function findMatching(array, driverName) {
+    const found = array.filter(function (name) {
+        return name.toLowerCase() === driverName.toLowerCase()
+    })
+    return found
+}
+
+function fuzzyMatch (array, driverName) {
+const found = array.filter(function (name) {
+    return name[0]+name[1]+name[2]=== driverName[0]+name[1]+name[2]
+})
+    return found
+}
+
+function matchName (array, string) {
+    const found = array.filter(function (driver) {
+        return driver.name === string
+    })
+    return found
+}
